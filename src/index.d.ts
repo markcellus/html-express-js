9c8be9940a583d6d7a98e55d6dfb160638c0ca27
mark <1446916+markcellus@users.noreply.github.com>
2024-03-27 15:05:09 -0400

    Auto build Typescript file when versioning project

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,35 @@
-import htmlExpress, { staticIndexHandler } from './index.js';
-
-export default htmlExpress;
-export const staticIndexHandler = staticIndexHandler;
-export const html = html;
+/**
+ * Template literal that supports string interpolating in passed HTML.
+ * @param {*} strings
+ * @param  {...any} data
+ * @returns {string} - HTML string
+ */
+export function html(strings: any, ...data: any[]): string;
+/**
+ * Attempts to render index.js pages when requesting to
+ * directories and fallback to 404/index.js if doesnt exist.
+ *
+ * @param {object} [options]
+ * @param {object} options.viewsDir - The directory that houses any potential index files
+ * @param {string} [options.notFoundView] - The path of a file relative to the views
+ *    directory that should be served as 404 when no matching index page exists. Defaults to `404/index`.
+ * @returns {import('express').RequestHandler} - Middleware function
+ */
+export function staticIndexHandler(options?: {
+  viewsDir: object;
+  notFoundView?: string;
+}): import('express').RequestHandler;
+/**
+ * Returns a template engine view function.
+ *
+ * @param {object} [opts]
+ * @param {object} [opts.includesDir]
+ * @returns {(path: string, options: object, callback: (e: any, rendered?: string) => void) => void}
+ */
+export default function _default(opts?: {
+  includesDir?: object;
+}): (
+  path: string,
+  options: object,
+  callback: (e: any, rendered?: string) => void,
+) => void;
